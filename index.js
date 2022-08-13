08d8d729b51f59bdba24b9fa9b2affb6287499fd
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-08-14 01:41:12 +0530

    More eficient way to use route-level middleware

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require("express");
 const path = require("path");
 const ageMiddleware = require("./middleware");
-
+const route = express.Router();
 const app = express();
 
 const pagePath = path.join(__dirname, 'pages');
 
+route.use(ageMiddleware);
 
-app.get('', ageMiddleware, (req, res) => {
+app.get('', (req, res) => {
     res.sendFile(`${pagePath}/index.html`);
 });
 
-app.get('/about', ageMiddleware, (req, res) => {
+route.get('/about', (req, res) => {
     res.sendFile(`${pagePath}/about.html`);
 });
 
-app.get('/contact', ageMiddleware, (req, res) => {
+route.get('/contact', (req, res) => {
     res.sendFile(`${pagePath}/contact.html`);
 });
+
+app.use('/', route);
 
 app.listen(4000);
