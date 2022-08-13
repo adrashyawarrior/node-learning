6d00f905d61e19a1587689949863868b94dea7c3
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-08-14 01:30:52 +0530

    application  level middleware used

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const app = express();
 
 const pagePath = path.join(__dirname, 'pages');
 
+// middleware
+const ageMiddleware = (req, res, next) => {
+    if (!req.query.age) {
+        res.send("Please provide your age.");
+    } else if (parseInt(req.query.age) < 18) {
+        res.send("Only 18+ users allwed.");
+    } else {
+        next();
+    }
+}
+app.use(ageMiddleware);
+
 app.get('', (req, res) => {
     res.sendFile(`${pagePath}/index.html`);
 });
