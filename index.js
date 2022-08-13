6e530ddc54637f112c4a427cbd5cc8f316b241cd
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-08-13 21:59:09 +0530

    Route Testing

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ app.get('', (req, res) => {
     res.send("Hello World!");
 });
 
+app.get('/about', (req, res)=>{
+    res.send("Hello From About Us!");
+});
+
+app.get('/contact', (req, res)=>{
+    res.send("Hello From Contact Us!");
+});
+
 app.listen(4000);
