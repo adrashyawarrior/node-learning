8afa829d67c1b926c68b891f6b25670a2722f6d7
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-08-14 07:51:49 +0530

    Mongo DB Connect Finally

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const { MongoClient } = require("mongodb")
-const url = "mongodb+srv://lalit:8gGQeRMMmhJlALfd@cluster0.otjpmcp.mongodb.net/test?retryWrites=true&w=majority";
 
-const database = "test";
+// Connection String
+// const url = 'mongodb://localhost:27017';
+const url = "mongodb+srv://lalit:8gGQeRMMmhJlALfd@cluster0.otjpmcp.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(url);
-async function getData() {
-    let conn = await client.connect();
-    let db = conn.db(database);
+
+// Database Name
+const database = "test";
+
+
+async function main() {
+    // Connect
+    await client.connect();
+    console.log("Connected to Mongo Server Successfully.");
+
+    // Select Database
+    let db = client.db(database);
+
+    // Select Collection
     let collection = db.collection('User');
+
+    // Apply Query
     let result = await collection.find({}).toArray();
+
+    // Printing Result
     console.log(result);
+
+    return "Done";
 }
 
-getData();
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => { client.close() });
