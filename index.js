d683d2cbd004d383ee412ca0e04b7ccfc1166956
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-08-14 19:06:52 +0530

    File Upload Done

--- index.js
+++ index.js
@@ -1,56 +1,17 @@
-require("./config")
 const express = require("express")
-const Product = require("./models/product")
-
+const multer = require("multer")
 
 const app = express();
 
-app.use(express.json());
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) { cb(null, "uploads") },
+        filename: function (req, file, cb) { cb(null, file.fieldname + "-" + Date.now() + ".jpg") }
+    })
+}).single("avatar");
 
-app.get('', async (req, res) => {
-    const products = await Product.find();
-    res.send(products);
-});
-
-app.post('', async (req, res) => {
-    let data = req.body;
-    let product = new Product(data);
-    product = await product.save();
-    data = {
-        success: true,
-        message: "Product Created Successfully.",
-        data: product
-    };
-    res.send(data);
-});
-
-app.get('/search/:key', async (req, res) => {
-    let products = await Product.find({
-        $or: [
-            { name: { $regex: req.params.key } },
-            { brand: { $regex: req.params.key } }
-        ]
-    });
-    res.send(products);
-});
-
-app.delete('/:_id', async (req, res) => {
-    await Product.deleteOne(req.params);
-    data = {
-        success: true,
-        message: "Product Deleted Successfully."
-    };
-    res.send(data);
-});
-
-app.put('/:_id', async (req, res) => {
-    let product = await Product.updateOne(req.params, { $set: req.body });
-    data = {
-        success: true,
-        message: "Product Updated Successfully.",
-        data: product
-    };
-    res.send(data);
+app.post('/upload', upload, async (req, res) => {
+    res.send("File Uploaded!");
 });
 
 app.listen(4000);
