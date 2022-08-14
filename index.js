32f4fe8b0ba9ba87589e99d1e36d0c259e81691d
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-08-14 19:19:15 +0530

    OS Module uses

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-const express = require("express")
-const multer = require("multer")
+const os = require("os");
 
-const app = express();
-
-const upload = multer({
-    storage: multer.diskStorage({
-        destination: function (req, file, cb) { cb(null, "uploads") },
-        filename: function (req, file, cb) { cb(null, file.fieldname + "-" + Date.now() + ".jpg") }
-    })
-}).single("avatar");
-
-app.post('/upload', upload, async (req, res) => {
-    res.send("File Uploaded!");
-});
-
-app.listen(4000);
+console.log(os.arch());
+console.log(os.freemem() / (1024 * 1024 * 1024))
+console.log(os.totalmem() / (1024 * 1024 * 1024))
+console.log(os.hostname());
+console.log(os.platform());
+console.log(os.userInfo());
