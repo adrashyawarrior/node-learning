ccda8c452e10184049b5effb2b319962adb93df5
adrashyawarrior <lalitkumar9557758955@gmail.com>
2022-08-14 23:32:39 +0530

    404 handling

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const productRoutes = require("./routes/product-routes")
 
 const app = express();
 app.use(productRoutes);
+
+// 404
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page Not Found</h1>");
+});
 app.listen(4000)
